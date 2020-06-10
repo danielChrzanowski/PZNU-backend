9ab2472aa47220bbbfe0c75e554dad28af7702f9
Daniel <daniel_chrzanowski98@wp.pl>
2020-06-10 19:03:29 +0200

    select last 6 posts

--- src/main/java/pznuBackend/repositories/PostRepository.java
+++ src/main/java/pznuBackend/repositories/PostRepository.java
@@ -1,10 +1,15 @@
 package pznuBackend.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pznuBackend.model.Post;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends CrudRepository<Post, Integer> {
+    @Query(value = "select * from PZNU_POSTS order by id desc limit 6", nativeQuery = true)
+    List<Post> findAll();
 
 }

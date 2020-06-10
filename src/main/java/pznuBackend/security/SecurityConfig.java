fd8b52a3efe44e5a05e8e68b442dfa63ec70cbd9
rafallyczek <rafal.lyczek@gmail.com>
2020-06-10 16:39:30 +0200

    added deleteLink to authenticated paths

--- src/main/java/pznuBackend/security/SecurityConfig.java
+++ src/main/java/pznuBackend/security/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/userByIdNoPassword","/user","/passwordById","/changePassword","/deleteUser","/getAllMojeZadania","/addMojeZadanie").authenticated()
+                .antMatchers("/userByIdNoPassword","/user","/passwordById","/changePassword","/deleteUser","/getAllMojeZadania","/addMojeZadanie","/deleteLink").authenticated()
                 .antMatchers("/", "/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
